8becca2f79d7b76bb20e249fc913848d753a7c15
vknue <valonknue@outlook.com>
2023-05-30 23:30:18 +0200

    Outrageous comment added

--- IntegrationModule/Program.cs
+++ IntegrationModule/Program.cs
@@ -7,7 +7,7 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+//This is an outrageous comment
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
